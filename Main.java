aa5b07a465bf696f37aca6ced8b195ef92aaf4d3
mylennabra <mylenna.bra@gmail.com>
2022-06-10 15:51:03 -0300

    refactorandfeat(Melhor representação de botões, adição de botão sobre da barra de ferramentas e check de campos vazios)

--- Main.java
+++ Main.java
@@ -5,14 +5,25 @@ import java.awt.event.ActionListener;
 
                                 // Adicionando interface
 public class Main extends JFrame implements ActionListener{ 
-
+    
     // l para Label e Nome com N maiúsculo.... Criar objeto com nome parecido com aquilo que será usado
     JLabel lValor1, lValor2, lValor3, lResult;
     JTextField tfValor1, tfValor2, tfValor3, tfResult;
-    JButton bPoten, bMultP, bDivP, bLimpar, bSair, miSobre;
+    JButton bSoma, bSub, bMult, bDiv, bLimpar, bSair;
     JTextArea taHistorico; 
     JCheckBox cbHistorico;
-
+    
+    // Criando barra
+    JMenuBar mbmenuBar = new JMenuBar();
+    // Adicionando itens
+    JMenu versionMenu = new JMenu("Info");
+    JMenu sobreMenu = new JMenu("Mais");
+    // Adicionando subitens aos itens da bar
+    JMenuItem authorAction = new JMenuItem("Mylenna");
+    JMenuItem versionAction = new JMenuItem("V 1.0 - 2022");
+    JMenuItem sobreAction = new JMenuItem("Clique para saber mais");
+    
+    
     Calculadora c = new Calculadora(); // Adicionando classe Calculadora como escopo global 
 
 
@@ -23,19 +34,10 @@ public class Main extends JFrame implements ActionListener{
         setVisible(true); // Torna o frame visivel
         setTitle("Calculadora"); // Definindo título pra janela 
 
-        // Criando barra
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
-        // Adicionando itens
-        JMenu versionMenu = new JMenu("Info");
-        JMenu sobreMenu = new JMenu("Mais");
         // Adicionando itens ao frame
-        menuBar.add(versionMenu);
-        menuBar.add(sobreMenu);
-        // Adicionando subitens aos itens da bar
-        JMenuItem authorAction = new JMenuItem("Mylenna");
-        JMenuItem versionAction = new JMenuItem("V 1.0 - 2022");
-        JMenuItem sobreAction = new JMenuItem("Clique para saber mais");
+        setJMenuBar(mbmenuBar);
+        mbmenuBar.add(versionMenu);
+        mbmenuBar.add(sobreMenu);
 
         versionMenu.add(authorAction);
         versionMenu.addSeparator();
@@ -47,8 +49,7 @@ public class Main extends JFrame implements ActionListener{
         tfValor1.setToolTipText("Inserir o 1° valor"); // Adicionando descrição on hover
         lValor2 = new JLabel("Potencia: "); 
         tfValor2 = new JTextField(5); 
-        lValor3 = new JLabel("Segunda potencia: "); 
-        tfValor3 = new JTextField(5); 
+
 
         lResult = new JLabel("Resultado: "); 
         tfResult = new JTextField(10); 
@@ -59,40 +60,38 @@ public class Main extends JFrame implements ActionListener{
         add(tfValor1); // Adicionando o objeto na frame
         add(lValor2); 
         add(tfValor2); 
-        add(lValor3); 
-        add(tfValor3); 
         add(lResult); 
         add(tfResult); 
         
         ////////////////////////////////////////////////////////////////////////
 
-        bPoten = new JButton("Potencia");
-        bMultP = new JButton("Multiplicar potencias");
-        bDivP = new JButton("Dividir potencias");
+        bSoma = new JButton("+");
+        bSub = new JButton("-");
+        bMult = new JButton("x");
+        bDiv = new JButton("/");
         bLimpar = new JButton("Limpar");
         bSair = new JButton("Sair");
-        // !!-----------------kkjkj
-        miSobre = new JButton("Sobre");
         taHistorico = new JTextArea(15,15);
         taHistorico.setEditable(false);
         cbHistorico = new JCheckBox("Memorizar");
         
-        bPoten.addActionListener(this);
-        bMultP.addActionListener(this);
-        bDivP.addActionListener(this);
+        bSoma.addActionListener(this);
+        bSub.addActionListener(this);
+        bMult.addActionListener(this);
+        bDiv.addActionListener(this);
         bLimpar.addActionListener(this);
         bSair.addActionListener(this);
-        miSobre.addActionListener(this);
+        sobreAction.addActionListener(this);
         
-        add(bPoten);
-        add(bMultP);
-        add(bDivP);
+        add(bSoma);
+        add(bSub);
+        add(bMult);
+        add(bDiv);
         add(bLimpar);
         add(bSair);
         add(taHistorico);
         add(cbHistorico);
-        // !!-----------------kkjkj
-        add(miSobre);
+
 
     }
 
@@ -110,48 +109,73 @@ public class Main extends JFrame implements ActionListener{
         if(ae.getSource()==bLimpar){
             tfValor1.setText("");
             tfValor2.setText("");
-            tfValor3.setText("");
             tfResult.setText("");
             taHistorico.setText("");
         }
-        if(ae.getSource()==bPoten){
-            int number = Integer.parseInt(tfValor1.getText());
-            int power  = Integer.parseInt(tfValor2.getText());
+        if(ae.getSource()==bSoma){
+            if(tfValor1.getText().length() > 0 && tfValor2.getText().length() > 0 ){
+                int num1 = Integer.parseInt(tfValor1.getText());
+                int num2  = Integer.parseInt(tfValor2.getText());
 
-            double result = c.potencia(number,power);
+                int result = c.soma(num1,num2);
 
-            tfResult.setText(""+result); //parte que vai gravar no historico
-            if(cbHistorico.isSelected()){
-                taHistorico.setText(taHistorico.getText() + number+"^"+power+"="+result + "\n");
+                tfResult.setText(""+result); //parte que vai gravar no historico
+                if(cbHistorico.isSelected()){
+                    taHistorico.setText(taHistorico.getText() + num1+"+"+num2+"="+result + "\n");
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "Preencha todos os campos", "Warning", JOptionPane.INFORMATION_MESSAGE);
             }
         }
 
-        if(ae.getSource()==bMultP){
-            int number = Integer.parseInt(tfValor1.getText()); //pegando valores
-            int power  = Integer.parseInt(tfValor2.getText());
-            int power2  = Integer.parseInt(tfValor3.getText());
+        if(ae.getSource()==bSub){
+            if(tfValor1.getText().length() > 0 && tfValor2.getText().length() > 0 ){
+                int num1 = Integer.parseInt(tfValor1.getText()); //pegando valores
+                int num2  = Integer.parseInt(tfValor2.getText());
 
-            int result = c.multPot(power, power2); // usando a função 
+                int result = c.subtrai(num1, num2); // usando a função 
 
-            tfResult.setText(""+number+"^"+result); //parte que vai gravar no historico
-            if(cbHistorico.isSelected()){
-                taHistorico.setText(taHistorico.getText() + number+"^"+power+"x"+number+"^"+power2+" = "+number+"^"+ result+"\n");
+                tfResult.setText(""+result); //parte que vai gravar no historico
+                if(cbHistorico.isSelected()){
+                    taHistorico.setText(taHistorico.getText() + num1+"-"+num2+" = "+result+"\n");
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "Preencha todos os campos", "Warning", JOptionPane.INFORMATION_MESSAGE);
             }
         }
 
-        if(ae.getSource()==bDivP){
-            int number = Integer.parseInt(tfValor1.getText());
-            int power  = Integer.parseInt(tfValor2.getText());
-            int power2  = Integer.parseInt(tfValor3.getText());
+        if(ae.getSource()==bMult){
+            if(tfValor1.getText().length() > 0 && tfValor2.getText().length() > 0 ){
+                int num1 = Integer.parseInt(tfValor1.getText());
+                int num2  = Integer.parseInt(tfValor2.getText());
 
-            int result = c.divPot(power, power2);
+                int result = c.multiplica(num1, num2);
 
-            tfResult.setText(""+number+"^"+result); //parte que vai gravar no historico
-            if(cbHistorico.isSelected()){
-                taHistorico.setText(taHistorico.getText() + number+"^"+power+"x"+number+"^"+power2+" = "+number+"^"+ result+"\n");
+                tfResult.setText(""+result); //parte que vai gravar no historico
+                if(cbHistorico.isSelected()){
+                    taHistorico.setText(taHistorico.getText() + num1+" x "+num2+" = "+result+"\n");
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "Preencha todos os campos", "Warning", JOptionPane.INFORMATION_MESSAGE);
             }
         }
-        if(ae.getSource()==miSobre){
+
+        if(ae.getSource()==bDiv){
+            if(tfValor1.getText().length() > 0 && tfValor2.getText().length() > 0 ){
+                int num1 = Integer.parseInt(tfValor1.getText());
+                int num2  = Integer.parseInt(tfValor2.getText());
+
+                float result = c.divide(num1, num2);
+
+                tfResult.setText(""+result); //parte que vai gravar no historico
+                if(cbHistorico.isSelected()){
+                    taHistorico.setText(taHistorico.getText() + num1+"/"+num2+ result+"\n");
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "Preencha todos os campos", "Warning", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+        if(ae.getSource()==sobreAction){
             new Sobre();
         }
     }
