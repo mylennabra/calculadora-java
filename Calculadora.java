13ef21557777dc28f2de4c61afe5fcd09a12ac54
mylennabra <mylenna.bra@gmail.com>
2022-06-10 15:48:40 -0300

    fix(Operações tradicionais adicionadas)

--- Calculadora.java
+++ Calculadora.java
@@ -2,18 +2,20 @@ import java.text.DecimalFormat;
 
 public class Calculadora {
     
-    public double potencia (int num, int pow){
-        return Math.pow(num,pow);
+    public int soma (int a, int b){
+        return (a+b);
     }
 
-    public int multPot (int pow, int pow2){
-        return pow+pow2;
+    public int subtrai (int a, int b){
+        return (a-b);
     }
 
-    public int divPot (int pow, int pow2){
-        return pow-pow2;
+    public int multiplica (int a, int b){
+        return (a*b);
     }
 
-
+    public float divide (int a, int b){
+        return a/b;
+    }
 
 }
